508ffc28f899acc2e889e3be3272f0c2f0699696
Oleg Shkola <olsh91@gmail.com>
2021-04-02 19:42:08 +0300

    Create count_tokens.cpp

---
+++ count_tokens.cpp
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> splitToTokens(string& input)
+{
+    istringstream ss(input);
+    vector<string> tokens;
+    
+    for(std::string singleToken; ss >> singleToken; ) 
+        tokens.push_back(singleToken);    
+
+    return tokens;
+}
+
+void countUniqueTokens(string& input)
+{
+    vector<string> tokens = splitToTokens(input);
+    
+    sort(tokens.begin(), tokens.end());
+    tokens.erase(std::unique(tokens.begin(), tokens.end()), tokens.end());
+    
+    cout << "N of unique words: " << tokens.size() << endl;
+}
+
+
+int main()
+{
+    string input;
+    getline(cin, input);
+    countUniqueTokens(input);
+}
